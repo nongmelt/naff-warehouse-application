a8703f63371d3d98eebbf5521728733bc404dc18
nongmelt <pongporn.supa@gmail.com>
2026-05-06 01:21:21 +0700

    chore: remove legacy backend from repo

--- backend-legacy/Data/AppDbContext.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-
-namespace backend.Data;
-
-public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
-{
-    public DbSet<PackingListEntity> PackingLists => Set<PackingListEntity>();
-}
